8de877c30df6e8734bf27ce33e06492c7f06cdd0
jaegon-kim <jaegon.kim@gmail.com>
2023-06-19 08:16:20 +0000

    adding primitive test

--- src/test/primitives.rs
+++ src/test/primitives.rs
@@ -1,3 +1,20 @@
+#![allow(unused)]
 pub fn test_primitive() {
-    println!("test primitive");
+    println!("test primitive(no warning)");
+    let logical: bool = true;
+    let a_logical: f64 = 1.0;
+    let an_integer = 5i32;
+
+    let default_float = 1.0;
+    let default_integer = 7;
+
+    let mut inferred_int = 12;
+    inferred_int = 10000000i64;
+
+    let mut mutable = 12;
+    mutable = 60;
+
+    //mutable = true; // this causes error
+    let mutable = true; // this causes warning   
+
 }
