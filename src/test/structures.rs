fd13dc19fefece5e957a8740164075ba1b2d41aa
jaegon-kim <jaegon.kim@gmail.com>
2023-07-20 05:16:05 +0000

    structure printing examples

--- src/test/structures.rs
+++ src/test/structures.rs
@@ -17,8 +17,9 @@ struct Point {
     y: f32,
 }
 
+#[derive(Debug)]
 struct Rectangle {
-    top_lef: Point,
+    top_left: Point,
     bot_right: Point,
 }
 
@@ -35,6 +36,31 @@ pub fn test_structures() {
 
     let point = Point { x: 10.0, y: 11.0};
     println!(" point: {:?}", point);
+
+    let point2 = Point { x:20.0, ..point };
+    println!(" -");
+    println!(" point : {:?}", point);
+    println!(" point2: {:?}", point2);
+    println!(" point2.x: {}, point2.y: {}", point2.x, point2.y);
+
+    let Point { x: left_edge, y: top_edge } = point;
+    println!(" point : {:?}, left_edge: {}, top_edge: {}",
+        point, left_edge, top_edge);
+
+    let rect = Rectangle {
+        top_left: Point { x: left_edge, y: top_edge },
+        bot_right: point2,
+    };
+
+    println!("rect: {:?}", rect);
+
+    let unit = Unit;
+
+    let pair = Pair (100, 101.0);
+    println!("pair: {} {}", pair.0, pair.1);
+
+    let Pair (integer, float) = pair;
+    println!("integer: {}, float: {}", integer, float);
 
 }
 
