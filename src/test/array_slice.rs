51281d61591873058c4a782aefdb5197b8c61091
jaegon-kim <jaegon.kim@gmail.com>
2023-07-20 01:07:12 +0000

    for loop

--- src/test/array_slice.rs
+++ src/test/array_slice.rs
@@ -5,6 +5,14 @@ fn analyze_slice(slice: &[i32]) {
     println!(" slice[0] = {}, len: {}", slice[0], slice.len());
 }
 
+fn print_slice(slice: &[i32]) {
+    print!("print_slice ");
+    print!("[");
+    for v in slice {
+        print!(" {}", v);
+    }
+    print!(" ]\n");
+}
 
 pub fn test_array_slice() {
     println!("test_array_slice");
@@ -24,5 +32,35 @@ pub fn test_array_slice() {
     let empty_array: [u32; 0] = [];
     assert_eq!(&empty_array, &[]);
     assert_eq!(&empty_array, &[][..]);
+
+    println!();
+    for i in 0..5 {
+        println!(" {}", i);
+    }
+
+    println!();
+    for i in 0..xs.len() {
+        println!(" {}", i);
+    }
+
+    println!();
+    for v in xs {
+        println!(" {}", v);
+    }
+
+    print_slice(&xs);
+    print_slice(&xs[1..xs.len()-1]);
+    print_slice(&xs[1..xs.len()]);
+    //print_slice(&ys);
+    print_slice(&ys[10..12]);
+
+    for i in 0..xs.len() + 1 {
+        match xs.get(i) {
+            Some(xval) => println!(" {}: {}", i, xval),
+            None => println!(" {} is None", i),
+        }
+    }
+
+
 }
 
