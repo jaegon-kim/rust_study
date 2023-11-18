8592b069cbfc895577ab26ef25a63e0d8e57f7e0
jaegon-kim <jaegon.kim@gmail.com>
2023-11-19 07:25:14 +0900

    test enums

--- src/test/enums.rs
+++ src/test/enums.rs
@@ -9,17 +9,34 @@ enum WebEvent {
 }
 
 fn inspect(event: WebEvent) {
-    /*
-    match(event) {
-        WebEvent::PageLoad => println!("PageLoad"),
-        WebEvent::PageUnload => println!("PageUnload"),
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        // Destructure `c` from inside the `enum` variant.
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        // Destructure `Click` into `x` and `y`.
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        },
     }
-    */
-
 }
 
 
 pub fn test_enum() {
     println!("test_enum");
+
+    let pressed = WebEvent::KeyPress('x');
+    // `to_owned()` creates an owned `String` from a string slice.
+    let pasted  = WebEvent::Paste("my text".to_owned());
+    let click   = WebEvent::Click { x: 20, y: 80 };
+    let load    = WebEvent::PageLoad;
+    let unload  = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
 }
 
