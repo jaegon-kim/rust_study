baab6a43103228271f21750ddff65fc66da5924e
jaegon-kim <jaegon.kim@gmail.com>
2023-11-19 07:44:39 +0900

    enum use & c-like

--- src/test/enums.rs
+++ src/test/enums.rs
@@ -23,6 +23,30 @@ fn inspect(event: WebEvent) {
 }
 
 
+enum Status {
+    Rich,
+    Poor,
+}
+
+enum Work {
+    Civilian,
+    Soldier,
+}
+
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+// enum with explicit discriminator
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+
 pub fn test_enum() {
     println!("test_enum");
 
@@ -38,5 +62,36 @@ pub fn test_enum() {
     inspect(click);
     inspect(load);
     inspect(unload);
+
+        // Explicitly `use` each name so they are available without
+    // manual scoping.
+    use Status::{Poor, Rich};
+    // Automatically `use` each name inside `Work`.
+    use Work::*;
+
+    // Equivalent to `Status::Poor`.
+    let status = Poor;
+    // Equivalent to `Work::Civilian`.
+    let work = Civilian;
+
+    match status {
+        // Note the lack of scoping because of the explicit `use` above.
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        // Note again the lack of scoping.
+        Civilian => println!("Civilians work!"),
+        Soldier  => println!("Soldiers fight!"),
+    }
+
+    // `enums` can be cast as integers.
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+
 }
 
