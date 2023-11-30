9b863803d0c78a45728ac655fce216dd07727ea1
jaegon-kim <jaegon.kim@gmail.com>
2023-12-01 08:56:40 +0900

    struct visibility

--- src/test/modules.rs
+++ src/test/modules.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 
 
+// Modules - Visibility 
+// https://doc.rust-lang.org/rust-by-example/mod/visibility.html
+
 mod my_mod {
     fn private_function() {
         println!("called test::modules::my_mod::private_function()");
@@ -11,6 +14,7 @@ mod my_mod {
         private_function();
     }
 
+    // public netsted module
     pub mod nested {
 
         pub fn function() {
@@ -48,6 +52,7 @@ mod my_mod {
         println!("called `my_mod::public_function_in_crate()`");
     }
 
+    // private nested module
     mod private_nested {
         #[allow(dead_code)]
         pub fn function() {
@@ -61,23 +66,74 @@ mod my_mod {
             println!("called `my_mod::private_nested::restricted_function()`");
         }
     }
-
-
 }
-
 
 fn function() {
     println!("called `function()`");
 }
 
-pub fn test_modules() {
-    println!("test_modules");
+
+fn test_visibility() {
+    println!("* test_visibility");
     function();
     my_mod::pub_function();
     //test::modules::my_mod::pub_function();
-
     my_mod::nested::function();
     my_mod::call_public_function_in_my_mod();
     my_mod::public_function_in_crate();
+    // error - below is a private function
+    //my_mod::nested::public_function_in_my_mod();
+    // error - below is a private function
+    //my_mod::private_function();
+    // errors - belows are a private module access
+    //my_mod::private_nested::function();
+    //my_mod::private_nested::restricted_function();
 
+}
+
+// Module - Struct Visibility
+// https://doc.rust-lang.org/rust-by-example/mod/struct_visibility.html
+
+mod my {
+    pub struct OpenBox<T> {
+        pub contents: T, // public field of generic type 'T'
+    }
+
+    pub struct ClosedBox<T> {
+        contents: T, // private field of generic type 'T'
+    }
+
+    impl<T> ClosedBox<T> {
+        pub fn new(contents: T) -> ClosedBox<T> {
+            ClosedBox {
+                contents: contents,
+            }
+        }
+    }
+}
+
+fn test_struct_visibility() {
+    println!("* test_struct_visibility");
+
+    let open_box = my::OpenBox {
+        contents: "public information"
+    };
+
+    println!("The open box contains: {}", open_box.contents);
+    
+    // error! ClosedBox.contents is a private field
+    //let closed_box = my::ClosedBox { 
+    //    contents: "classified information" 
+    //};
+
+    let _closed_box = my::ClosedBox::new("classified information");
+    // error _closed_box.contents is a private field
+    //println!("The closed box contains: {}", _closed_box.contents);
+
+}
+
+
+pub fn test_modules() {
+    test_visibility();
+    test_struct_visibility();
 }
